e1994b2bdda76507c798f7cf50d4bac6de9a71ed
Ayub4702 <nadaara5538@gmail.com>
2026-01-07 08:31:31 +0200

    exercise 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
  
-import ShoppingCart from "./ShoppingCart"
-import UseEffecr from "./UseEffecr"
-import Exercise from "./Exercise"
-import TimerUseEffects from "./TimerUseEffect"
-import PokemanApi from "./PokemanApi"
+// import ShoppingCart from "./ShoppingCart"
+// import UseEffecr from "./UseEffecr"
+// import Exercise from "./Exercise"
+// import TimerUseEffects from "./TimerUseEffect"
+// import PokemanApi from "./PokemanApi"
+// import Usercard from "./Usercard"
+import UserList from "./UserList"
 
 function App (){
 
+  const users = [
+    {id: 1, name: "Ayuub", email: "nadaara5538@5a5.com"},
+    {id: 12, name: "yuusuf", email: "yuusufra58@5a5.com"}
+  ]
 return (
   <>
   
-  <ShoppingCart/>
+  {/* <ShoppingCart/>
   <UseEffecr/>
   <Exercise/>
   <TimerUseEffects/>
   <PokemanApi/>
-  
+  // <Usercard/> */}
+  <UserList users={users}/>  
+
   </>
 )
 
--- src/Usercard.jsx
+++ src/Usercard.jsx
@@ -1,12 +1,54 @@
- const Usercard = ({Email , Name})=> {
+import { useState, useEffect } from "react";
 
-  return <div>
+export default function TimeRunning() {
+  const [time, setTime] = useState(0);
+  const [choose, setChoose] = useState("");
+  const [running, setRunning] = useState(false);
 
-<h2>Email: {Email} </h2>
-<p>Name: {Name}</p>
+  useEffect(() => {
+    let timerId;
+    // const target = 0;
 
-  </div>
+    if (running && time > 0) {
+      timerId = setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000);
+    }
 
+    return () => clearInterval(timerId);
+  }, [running, time]);
+
+  const handleStart = () => {
+    setTime(time);
+    setRunning(true);
+    // console.log(time);
+  };
+
+  const handleStop = () => {
+    setRunning(false);
+  };
+
+  const handleReset = () => {
+    setRunning(false);
+  };
+
+  return (
+    <div>
+      <h1>Exercise Time Running</h1>
+      <div>
+        <span>Choose the seconds: </span>
+        <input
+          type="number"
+          value={choose}
+          onChange={(e) => setChoose(e.target.value)}
+        />
+      </div>
+      <h3>Stop Watch {choose} Seconds</h3>
+      <button onClick={handleStart} disabled={running || time}>Start</button>
+      <button onClick={handleStop} disabled={setRunning}>
+        Stop
+      </button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
 }
-
-export default Usercard;
